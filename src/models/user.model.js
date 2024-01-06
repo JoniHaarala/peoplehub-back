dc0bcd520ea3a49ae481477886d898669916cb6b
jonatan <jonatan.haaralaorosco@gmail.com>
2024-01-06 19:11:02 -0300

    import correctly mongoose

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,21 +1,22 @@
 import mongoose from 'mongoose'
+const { Schema } = mongoose
 
 // ejemplo de schema en mongoose
-const userSchema = mongoose.Schema({
+const userSchema = new Schema({
     username: {
         type: String,
         required: true, // igual al 'not null'
         trim: true //
     },
-    email: { 
+    email: {
         type: String,
-     },
-    password: { 
+    },
+    password: {
         type: String,
-     },
+    },
 })
 
-export default mongoose.model('User', userSchema); 
+export default mongoose.model('User', userSchema);
 
 /* 
     basicamente lo que hacemos es crear un documento que va dentro de la collecion USERS de modo que:
